785d5c38f10e71f719a0d4e688af1fa283755c6f
sangxxjin <sj_0368@naver.com>
2024-11-18 17:10:49 +0900

    feat(Main): 프로그램 실행
    본문:
    컨트롤러를 통해 프로그램을 실행하게 구현했습니다.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.example.controller.WiseSayingController;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
-
+        WiseSayingController controller = new WiseSayingController();
+        controller.run();
     }
 }
