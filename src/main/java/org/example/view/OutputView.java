ed7ca17c5a259e50c18f3819c9d7bc9468256f4d
sangxxjin <sj_0368@naver.com>
2024-11-19 15:01:50 +0900

    feat(CommandService): 명언 id값 출력 구현

--- src/main/java/org/example/view/OutputView.java
+++ src/main/java/org/example/view/OutputView.java
@@ -1,14 +1,22 @@
 package org.example.view;
 
 public class OutputView {
+
     public static final String START_MESSAGE = "== 명언 앱 ==";
     public static final String INPUT_PROMPT = "명령) ";
+    public static final String FINISH_REGISTER = "번 명언이 등록되었습니다.";
 
-    public static void showStartMessage(){
+
+    public void showStartMessage() {
         System.out.println(START_MESSAGE);
     }
-    public static void showInputPrompt(){
+
+    public void showInputPrompt() {
         System.out.print(INPUT_PROMPT);
+    }
+
+    public void showFinishRegister(int number) {
+        System.out.println(number + FINISH_REGISTER);
     }
 
 }
