4cd963a890109cc9205462eb0f3fd2b6068d188a
sangxxjin <sj_0368@naver.com>
2024-11-19 15:27:19 +0900

    feat(OutputView): 목록 리스트 출력 구현

--- src/main/java/org/example/view/OutputView.java
+++ src/main/java/org/example/view/OutputView.java
@@ -1,10 +1,15 @@
 package org.example.view;
 
+import java.util.List;
+import org.example.model.WiseSaying;
+
 public class OutputView {
 
     public static final String START_MESSAGE = "== 명언 앱 ==";
     public static final String INPUT_PROMPT = "명령) ";
     public static final String FINISH_REGISTER = "번 명언이 등록되었습니다.";
+    private static final String HEADER = "번호 / 작가 / 명언";
+    private static final String SEPARATOR = "----------------------";
 
 
     public void showStartMessage() {
@@ -17,6 +22,18 @@ public class OutputView {
 
     public void showFinishRegister(int number) {
         System.out.println(number + FINISH_REGISTER);
+    }
+
+    public void showList(List<WiseSaying> wiseSayings) {
+        System.out.println(HEADER);
+        System.out.println(SEPARATOR);
+        System.out.println(wiseSayings.size());
+        StringBuilder sb = new StringBuilder();
+        for (WiseSaying saying : wiseSayings) {
+            sb.append(saying.getId()).append(" / ").append(saying.getAuthor()).append(" / ")
+                .append(saying.getWiseSaying()).append("\n");
+        }
+        System.out.println(sb);
     }
 
 }
