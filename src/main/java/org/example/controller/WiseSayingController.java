b58b2e239a3a99a8611455fdcdf8d1d3073a95b1
sangxxjin <sj_0368@naver.com>
2024-11-19 16:31:25 +0900

    refactor(WiseSayingConroller): 컨벤션

--- src/main/java/org/example/controller/WiseSayingController.java
+++ src/main/java/org/example/controller/WiseSayingController.java
@@ -33,22 +33,19 @@ public class WiseSayingController {
 
     private void runCommand(String command) {
 
-        if(Command.valueOf("EXIT").getValue().equals(command)) {
+        if (Command.valueOf("EXIT").getValue().equals(command)) {
             wiseSayingService.exitSystem();
         }
-        if(Command.valueOf("REGISTER").getValue().equals(command)) {
+        if (Command.valueOf("REGISTER").getValue().equals(command)) {
             inputView.showSayingPrompt();
             String saying = inputView.getInput();
             inputView.showAuthorPrompt();
             String author = inputView.getInput();
             outputView.showFinishRegister(wiseSayingService.addWiseSaying(saying, author));
         }
-        if(Command.valueOf("LIST").getValue().equals(command)) {
+        if (Command.valueOf("LIST").getValue().equals(command)) {
             outputView.showList(wiseSayingService.getWiseSayings());
         }
-        //startsWith를 사용했는데 "삭제"라는 문구만 if문 안으로 진입하고
-        //"삭제~"와 같이 삭제 뒤에 무언가를 붙이면 if문 안으로 들어가지를 못하고있습니다.
-        //잠시분석좀할게요
         if (command.startsWith(Command.valueOf("DELETE").getValue())) {
             outputView.showFinishDelete(wiseSayingService.deleteWiseSaying(
                 inputView.extractDeleteWiseSayingId(command)));
