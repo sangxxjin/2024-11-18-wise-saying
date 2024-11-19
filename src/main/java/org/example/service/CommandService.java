b706c91af5d6028ec57cbcd108772c307761b1c9
sangxxjin <sj_0368@naver.com>
2024-11-19 12:30:42 +0900

    feat(CommandService): 커맨드를 기준으로 종료 추가하게 구현

--- src/main/java/org/example/service/CommandService.java
+++ src/main/java/org/example/service/CommandService.java
@@ -1,12 +1,15 @@
 package org.example.service;
 
-import org.example.enums.Command;
+import org.example.Repository.WiseSayingRepository;
 
 public class CommandService {
 
-    public void checkCommand(String command) {
-        if (Command.EXIT.getValue().equals(command)) {
-            System.exit(0);
-        }
+    public void exitSystem() {
+        System.exit(0);
+    }
+
+    public void addWiseSaying(String saying, String author) {
+        WiseSayingRepository wiseSayingRepository = new WiseSayingRepository();
+        wiseSayingRepository.addWiseSaying(saying, author);
     }
 }
