c40971e503e5cc6ef7437c45c0f3b63f58a8c431
sangxxjin <sj_0368@naver.com>
2024-11-19 12:32:16 +0900

    feat(): Controller에 view와 service를 의존성 주입 및 추가 기능 구현
    본문:
    controller에 view와 service를 의존성 주입 하였고 만들어 놓은 코드들을 기반으로 종료 및 등록하게 수정하였습니다.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,20 @@
 package org.example;
 
 import org.example.controller.WiseSayingController;
+import org.example.service.CommandService;
+import org.example.view.InputView;
+import org.example.view.OutputView;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
-        WiseSayingController controller = new WiseSayingController();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        CommandService commandService = new CommandService();
+        WiseSayingController controller = new WiseSayingController(inputView, outputView,
+            commandService);
         controller.run();
     }
 }
--- src/main/java/org/example/controller/WiseSayingController.java
+++ src/main/java/org/example/controller/WiseSayingController.java
@@ -1,16 +1,45 @@
 package org.example.controller;
 
 import org.example.service.CommandService;
+import org.example.validator.CommandValidator;
 import org.example.view.InputView;
 import org.example.view.OutputView;
 
 public class WiseSayingController {
-    public void run(){
-        OutputView.showStartMessage();
-        CommandService commandService = new CommandService();
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final CommandService commandService;
+
+    public WiseSayingController(InputView inputView, OutputView outputView,
+        CommandService commandService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.commandService = commandService;
+    }
+
+    public void run() {
+        outputView.showStartMessage();
         while (true) {
-            OutputView.showInputPrompt();
-            commandService.checkCommand(InputView.getInput());
+            outputView.showInputPrompt();
+            String command = inputView.getInput();
+            if (!CommandValidator.isExistCommand(command)) {
+                continue;
+            }
+            runCommand(command);
+        }
+    }
+
+    private void runCommand(String command) {
+        switch (command) {
+            case "종료":
+                commandService.exitSystem();
+            case "등록":
+                inputView.showSayingPrompt();
+                String saying = inputView.getInput();
+                inputView.showAuthorPrompt();
+                String author = inputView.getInput();
+                commandService.addWiseSaying(saying, author);
         }
     }
 }
