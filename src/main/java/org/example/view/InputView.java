eaa77fd91f5c4bbbc7b854d0e3adf411a2294764
sangxxjin <sj_0368@naver.com>
2024-11-19 12:28:39 +0900

    feat(InputView): 명언을 입력 받기 위한 출력 구현

--- src/main/java/org/example/view/InputView.java
+++ src/main/java/org/example/view/InputView.java
@@ -4,8 +4,19 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private final static Scanner sc = new Scanner(System.in);
-    public static String getInput(){
+    private final Scanner sc = new Scanner(System.in);
+    public static final String SAYING_PROMPT = "명언 : ";
+    public static final String AUTHOR_PROMPT = "작가 : ";
+
+    public String getInput() {
         return sc.nextLine();
+    }
+
+    public void showSayingPrompt() {
+        System.out.print(SAYING_PROMPT);
+    }
+
+    public void showAuthorPrompt() {
+        System.out.print(AUTHOR_PROMPT);
     }
 }
