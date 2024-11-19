006999e7d3d44b13352ce5f63ae9fe4c13c75302
sangxxjin <sj_0368@naver.com>
2024-11-19 15:27:04 +0900

    feat(CommandService): 목록 리스트 get 구현

--- src/main/java/org/example/service/CommandService.java
+++ src/main/java/org/example/service/CommandService.java
@@ -1,15 +1,24 @@
 package org.example.service;
 
+import java.util.List;
 import org.example.Repository.WiseSayingRepository;
+import org.example.model.WiseSaying;
 
 public class CommandService {
+    private final WiseSayingRepository wiseSayingRepository;
+
+    public CommandService() {
+        wiseSayingRepository = new WiseSayingRepository();
+    }
 
     public void exitSystem() {
         System.exit(0);
     }
 
     public int addWiseSaying(String saying, String author) {
-        WiseSayingRepository wiseSayingRepository = new WiseSayingRepository();
         return wiseSayingRepository.addWiseSaying(saying, author);
+    }
+    public List<WiseSaying> getWiseSayings() {
+        return wiseSayingRepository.getWiseSayings();
     }
 }
