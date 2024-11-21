80991d8f47e2dc817081ed72c33dbbe2cf341bb6
sangxxjin <sj_0368@naver.com>
2024-11-21 16:06:04 +0900

    refactor: 파일 생성 삭제와 같은 파일 CRUD repository로 변경

--- src/main/java/org/example/Repository/WiseSayingRepository.java
+++ src/main/java/org/example/Repository/WiseSayingRepository.java
@@ -1,8 +1,12 @@
 package org.example.Repository;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import org.example.model.WiseSaying;
 
 public class WiseSayingRepository {
@@ -24,7 +28,7 @@ public class WiseSayingRepository {
                 );
             }
         }
-        return null;
+        throw new NoSuchElementException("해당하는 명언을 찾을 수 없습니다.");
     }
 
     public List<WiseSaying> getWiseSayings() {
@@ -63,6 +67,70 @@ public class WiseSayingRepository {
             }
         }
         return maxId;
+    }
+
+    public void saveToFile(int id, String saying, String author) throws IOException {
+        String json = "{\n" +
+            "  \"id\": " + id + ",\n" +
+            "  \"content\": \"" + saying + "\",\n" +
+            "  \"author\": \"" + author + "\"\n" +
+            "}";
+
+        String filePath = "src/main/java/org/example/db/wiseSaying/" + id + ".json";
+        try (FileWriter writer = new FileWriter(filePath)) {
+            writer.write(json);
+        }
+    }
+
+    public void deleteAllFiles() {
+        File directory = new File("src/main/java/org/example/db/wiseSaying");
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            try {
+                file.delete();
+            } catch (Exception e) {
+                System.err.println("Error deleting file: " + file.getName());
+            }
+        }
+    }
+
+    public void deleteWiseSayingFile(int id) {
+        String filePath = "src/main/java/org/example/db/wiseSaying/" + id + ".json";
+        File file = new File(filePath);
+        try {
+            file.delete();
+        } catch (Exception e) {
+            System.err.println("Error deleting file: " + file.getName());
+        }
+
+    }
+
+    public void saveLastWiseSayingIdToFile() throws IOException {
+        String filePath = "src/main/java/org/example/db/wiseSaying/lastId.txt";
+        int lastWiseSayingId = getLastWiseSayingId();
+        try (FileWriter writer = new FileWriter(filePath)) {
+            writer.write(String.valueOf(lastWiseSayingId));
+        }
+    }
+
+    public void saveDataFile(List<WiseSaying> wiseSayings) {
+        String filePath = "src/main/java/org/example/db/wiseSaying/data.json";
+        try (FileWriter writer = new FileWriter(filePath)) {
+            List<String> jsonList = new ArrayList<>();
+            for (WiseSaying wiseSaying : wiseSayings) {
+                String json = "{" +
+                    "\"id\": " + wiseSaying.getId() + "," +
+                    "\"content\": \"" + wiseSaying.getWiseSaying() + "\"," +
+                    "\"author\": \"" + wiseSaying.getAuthor() + "\"" +
+                    "}";
+                jsonList.add(json);
+            }
+            writer.write("[");
+            writer.write(String.join(",", jsonList));
+            writer.write("]");
+        } catch (IOException e) {
+            System.err.println("파일 저장 중 오류 발생: " + e.getMessage());
+        }
     }
 
 }
--- src/main/java/org/example/service/WiseSayingService.java
+++ src/main/java/org/example/service/WiseSayingService.java
@@ -1,9 +1,6 @@
 package org.example.service;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.example.Repository.WiseSayingRepository;
@@ -45,66 +42,23 @@ public class WiseSayingService {
     public boolean isExistWiseSaying(int id) {
         return wiseSayingRepository.isExistWiseSaying(id);
     }
-
-
     public void saveToFile(int id, String saying, String author) throws IOException {
-        String json = "{\n" +
-            "  \"id\": " + id + ",\n" +
-            "  \"content\": \"" + saying + "\",\n" +
-            "  \"author\": \"" + author + "\"\n" +
-            "}";
-
-        String filePath = "src/main/java/org/example/db/wiseSaying/" + id + ".json";
-        try (FileWriter writer = new FileWriter(filePath)) {
-            writer.write(json);
-        }
+        wiseSayingRepository.saveToFile(id, saying, author);
     }
 
     public void deleteAllFiles() {
-        File directory = new File("src/main/java/org/example/db/wiseSaying");
-        File[] files = directory.listFiles();
-        for (File file : files) {
-            try {
-                file.delete();
-            } catch (Exception e) {
-                System.err.println("Error deleting file: " + file.getName());
-            }
-        }
+        wiseSayingRepository.deleteAllFiles();
     }
     public void deleteWiseSayingFile(int id){
-        String filePath = "src/main/java/org/example/db/wiseSaying/" + id + ".json";
-        File file = new File(filePath);
-        try {
-            file.delete();
-        } catch (Exception e) {
-            System.err.println("Error deleting file: " + file.getName());
-        }
+        wiseSayingRepository.deleteWiseSayingFile(id);
 
     }
     public void saveLastWiseSayingIdToFile() throws IOException {
-        String filePath = "src/main/java/org/example/db/wiseSaying/lastId.txt";
-        int lastWiseSayingId = wiseSayingRepository.getLastWiseSayingId();
-        try (FileWriter writer = new FileWriter(filePath)) {
-            writer.write(String.valueOf(lastWiseSayingId));
-        }
+        wiseSayingRepository.saveLastWiseSayingIdToFile();
     }
     public void saveDataFile(List<WiseSaying> wiseSayings) {
-        String filePath = "src/main/java/org/example/db/wiseSaying/data.json";
-        try (FileWriter writer = new FileWriter(filePath)) {
-            List<String> jsonList = new ArrayList<>();
-            for (WiseSaying wiseSaying : wiseSayings) {
-                String json = "{" +
-                    "\"id\": " + wiseSaying.getId() + "," +
-                    "\"content\": \"" + wiseSaying.getWiseSaying() + "\"," +
-                    "\"author\": \"" + wiseSaying.getAuthor() + "\"" +
-                    "}";
-                jsonList.add(json);
-            }
-            writer.write("[");
-            writer.write(String.join(",", jsonList));
-            writer.write("]");
-        } catch (IOException e) {
-            System.err.println("파일 저장 중 오류 발생: " + e.getMessage());
-        }
+        wiseSayingRepository.saveDataFile(wiseSayings);
     }
+
+
 }
