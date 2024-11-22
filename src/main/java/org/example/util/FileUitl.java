b334094ea7c61f1665b4b1cf4013fd5bcafed5c0
sangxxjin <sj_0368@naver.com>
2024-11-22 17:04:02 +0900

    refactor: 파일 경로 util화

---
+++ src/main/java/org/example/util/FileUitl.java
@@ -0,0 +1,16 @@
+package org.example.util;
+
+public class FileUitl {
+    public static String testFilePath(){
+        return "src/test/resources/db/wiseSaying/";
+    }
+    public static String originFilePath(){
+        return "src/main/resources/db/wiseSaying/";
+    }
+    public static String dataFileName(){
+        return "data.json";
+    }
+    public static String lastIdFileName(){
+        return "lastId.txt";
+    }
+}
